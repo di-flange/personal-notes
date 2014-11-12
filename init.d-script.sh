47fd88db8c3d28af7c0f230a13620e9de7bbf070
Anton Ishkov <iskov.anton@yandex.com>
2014-11-12 10:05:28 +0100

    Update init.d-script.sh

--- init.d-script.sh
+++ init.d-script.sh
@@ -1,18 +1,41 @@
 #!/bin/sh
 
-SERVICE_NAME=ProjectService
-RUN_COMMAND="/root/app/run.sh"
-PID_STORE=/tmp/project-service.pid
+SERVICE_NAME="Project name"
+SERVICE_ALIAS="project_name"
+LOCATION="/usr/lib/$SERVICE_ALIAS"
+LOG_LOCATION="/var/log/$SERVICE_ALIAS"
+RUN_COMMAND="run.command"
+PID_STORE="/tmp/$SERVICE_ALIAS.pid"
 
 case $1 in
     start)
         echo "Starting $SERVICE_NAME..."
-        if [ ! -f $PID_STORE ]; then
-            nohup $RUN_COMMAND 2>> /var/log/startup1.log >> /var/log/startup2.log &
+        if [ ! -f $PID_STORE ] || [ ! $(kill -s 0 "$(cat $PID_STORE)") ]; then
+            cd $LOCATION
+            nohup $RUN_COMMAND 2>> "$LOG_LOCATION/startup1.log" >> "$LOG_LOCATION/startup2.log" &
                         echo $! > $PID_STORE
-            echo "$SERVICE_NAME started ..."
+            echo "... started"
         else
-            echo "$SERVICE_NAME is already running ..."
+            echo "... is already running."
+        fi
+    ;;
+    status)
+        if [ -f $PID_STORE ]; then
+            PID=$(cat $PID_STORE);
+            
+            if [ ! -z "$PID" ]; then
+                STATE=$(ps -p $PID | grep $PID) > /dev/null
+            else 
+                STATE=''
+            fi
+
+            if [ ! -z "$STATE" ]; then
+                echo "$SERVICE_NAME is running (pid $PID)."
+            else
+                echo "$SERVICE_NAME is NOT running. (pid $PID - DIED UNEXPECTEDLY)"
+            fi
+        else
+            echo "$SERVICE_NAME is NOT running."
         fi
     ;;
     stop)
@@ -20,7 +43,7 @@ case $1 in
             PID=$(cat $PID_STORE);
             echo "$SERVICE_NAME stoping ..."
             kill $PID;
-            echo "$SERVICE_NAME stopped ..."
+            echo "... stopped"
             rm $PID_STORE
         else
             echo "$SERVICE_NAME is not running ..."
@@ -31,14 +54,16 @@ case $1 in
             PID=$(cat $PID_STORE);
             echo "$SERVICE_NAME stopping ...";
             kill $PID;
-            echo "$SERVICE_NAME stopped ...";
+            echo "... stopped";
+
             rm $PID_STORE
+
             echo "$SERVICE_NAME starting ..."
             nohup $RUN_COMMAND 2>> /var/astel/log/startup1.log >> /var/astel/log/startup2.log &
                         echo $! > $PID_STORE
-            echo "$SERVICE_NAME started ..."
+            echo "... started"
         else
             echo "$SERVICE_NAME is not running ..."
         fi
     ;;
-esac 
+esac
